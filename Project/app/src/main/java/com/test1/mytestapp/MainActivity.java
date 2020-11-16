310b453ccba3787e1744f43f074f040da6bde65e
Arsen <arsen10-10@mail.ru>
2020-11-16 22:04:01 +0400

    My second test

--- Project/app/src/main/java/com/test1/mytestapp/MainActivity.java
+++ Project/app/src/main/java/com/test1/mytestapp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // My test 1
+        // My test 2
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
